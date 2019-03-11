751a4dbf9b9ece66fa3b90b40a0ca833da54d200
webmilio <therealwebmilio@gmail.com>
2019-03-10 20:48:15 -0400

    Renamed DBT to DBTR.

--- Transformations/Transformation.cs
+++ Transformations/Transformation.cs
@@ -1,9 +1,36 @@
 ﻿using DBTMod;
+using Microsoft.Xna.Framework;
 
 namespace DBTMod.Transformations
 {
     public abstract class Transformation : IHasUnlocalizedName
     {
+        protected Transformation(string unlocalizedName)
+        {
+            UnlocalizedName = unlocalizedName;
+        }
 
+        #region Properties
+
+        public string UnlocalizedName { get; }
+
+
+        #region Statistics
+
+        #region Multipliers
+
+        public float BaseDamageMultiplier { get; }
+
+        public float BaseSpeedMultiplier { get; }
+
+        public float BaseDefenseMultiplier { get; }
+
+        #endregion
+
+
+
+        #endregion
+
+        #endregion
     }
 }
