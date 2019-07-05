bbdc7ee9f50698535bdaa93ab9bd5ff50af4673d
webmilio <therealwebmilio@gmail.com>
2019-07-04 20:00:32 -0400

    Added new donators.

--- Commons/Users/Developer.cs
+++ Commons/Users/Developer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DBT.Commons.Users
 {
-    public class Developer : User
+    public sealed class Developer : User
     {
         public Developer(long steamId64, string displayName, ulong discordId) : base(steamId64, displayName, discordId)
         {
--- Commons/Users/Donator.cs
+++ Commons/Users/Donator.cs
@@ -1,6 +1,6 @@
 ﻿namespace DBT.Commons.Users
 {
-    public class Donator : User
+    public sealed class Donator : User
     {
         public Donator(long steamId64, string displayName, ulong discordId) : base(steamId64, displayName, discordId)
         {
